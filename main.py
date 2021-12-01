344cdf72915cec2e5c9bf02665571fd88bcc55fa
MichaelHinrichs <mchinrichs@gmail.com>
2021-12-01 16:19:51 -0600

    Use f-strings

--- main.py
+++ main.py
@@ -55,22 +55,22 @@ def main():
             bom = ">" if inb[8:0xA] == b'\xFE\xFF' else "<" # Usually ">"
 
             endianness = {">": "Big", "<": "Little"}
-            print("Endianness: " + endianness[bom])
+            print(f"Endianness: {endianness[bom]}")
 
-            pos = struct.unpack(bom + "i", inb[0x4C:0x50])[0]
+            pos = struct.unpack(f"{bom}i", inb[0x4C:0x50])[0]
 
             pos += 0x4C
-            size = struct.unpack(bom + "I", inb[pos:pos+4])[0]
-            count = struct.unpack(bom + "i", inb[pos+4:pos+8])[0]
+            size = struct.unpack(f"{bom}I", inb[pos:pos+4])[0]
+            count = struct.unpack(f"{bom}i", inb[pos+4:pos+8])[0]
 
             if pos - 0x4C:
                 print("")
-                print("File count: " + str(count))
+                print(f"File count: {str(count)}")
             
 
                 for i in range(count + 1):
-                    name_pos = struct.unpack(bom + "i", inb[pos+16+(0x10*i):pos+20+(0x10*i)])[0]
-                    data_pos = struct.unpack(bom + "i", inb[pos+20+(0x10*i):pos+24+(0x10*i)])[0]
+                    name_pos = struct.unpack(f"{bom}i", inb[pos+16+(0x10*i):pos+20+(0x10*i)])[0]
+                    data_pos = struct.unpack(f"{bom}i", inb[pos+20+(0x10*i):pos+24+(0x10*i)])[0]
 
                     if data_pos in [0, 0xFFFFFFFF]:
                         name = ""
@@ -80,21 +80,21 @@ def main():
                         data_pos += pos + 8 + (0x10*i) + 12
 
                         pos = data_pos
-                        data_pos = struct.unpack(bom + "i", inb[pos:pos+4])[0] + pos
-                        dataSize = struct.unpack(bom + "I", inb[pos+4:pos+8])[0]
+                        data_pos = struct.unpack(f"{bom}i", inb[pos:pos+4])[0] + pos
+                        dataSize = struct.unpack(f"{bom}I", inb[pos+4:pos+8])[0]
 
                         data = inb[data_pos:data_pos + dataSize]
 
                         name = find_name(inb, name_pos)
                     
                         print("")
-                        print("File " + str(i) + " offset:       " + hex(data_pos))
-                        print("File " + str(i) + " size:         " + str(dataSize))
-                        print("File " + str(i) + " name offset:  " + hex(name_pos))
-                        print("File " + str(i) + " name:         " + name)
+                        print(f"File {str(i)} offset:       {hex(data_pos)}")
+                        print(f"File {str(i)} size:         {str(dataSize)}")
+                        print(f"File {str(i)} name offset:  {hex(name_pos)}")
+                        print(f"File {str(i)} name:         {name}")
 
                         folder = os.path.dirname(os.path.abspath(filename))
-                        with open(folder + "/" + name, "wb+") as output:
+                        with open(f"{folder}/{name}", "wb+") as output:
                             output.write(data)
 
             else:
@@ -107,10 +107,10 @@ def main():
             bom = ">" if inb[0xC:0xE] == b'\xFE\xFF' else "<" # Usually "<"
 
             endianness = {">": "Big", "<": "Little"}
-            print("Endianness: " + endianness[bom])
+            print(f"Endianness: {endianness[bom]}")
 
-            startoff = struct.unpack(bom + "q", inb[0x98:0xA0])[0]
-            count = struct.unpack(bom + "q", inb[0xC8:0xD0])[0]
+            startoff = struct.unpack(f"{bom}q", inb[0x98:0xA0])[0]
+            count = struct.unpack(f"{bom}q", inb[0xC8:0xD0])[0]
 
             if not count:
                 print("")
@@ -119,32 +119,32 @@ def main():
             else:
                 # Not sure if this is correct, I hope it is.
                 # If you face any problems, try replacing "0x20" with "0x10 * count".
-                namesoff = struct.unpack(bom + "q", inb[0xA0:0xA8])[0] + 0x20
+                namesoff = struct.unpack(f"{bom}q", inb[0xA0:0xA8])[0] + 0x20
 
                 print("")
-                print("File count: " + str(count))
+                print(f"File count: {str(count)}")
 
                 for i in range(count):
-                    fileoff = struct.unpack(bom + "q", inb[startoff + i * 16:startoff + 8 + i * 16])[0]
-                    dataSize = struct.unpack(bom + "q", inb[startoff + 8 + i * 16:startoff + 16 + i * 16])[0]
+                    fileoff = struct.unpack(f"{bom}q", inb[startoff + i * 16:startoff + 8 + i * 16])[0]
+                    dataSize = struct.unpack(f"{bom}q", inb[startoff + 8 + i * 16:startoff + 16 + i * 16])[0]
 
                     print("")
-                    print("File " + str(i+1) + " offset:       " + hex(fileoff))
-                    print("File " + str(i+1) + " size:         " + str(dataSize))
+                    print(f"File {str(i+1)} offset:       {hex(fileoff)}")
+                    print(f"File {str(i+1)} size:         {str(dataSize)}")
 
                     data = inb[fileoff:fileoff + dataSize]
 
-                    nameoff = struct.unpack(bom + "q", inb[namesoff + i * 16:namesoff + 8 + i * 16])[0]
+                    nameoff = struct.unpack(f"{bom}q", inb[namesoff + i * 16:namesoff + 8 + i * 16])[0]
 
-                    print("File " + str(i+1) + " name offset:  " + hex(nameoff))
+                    print(f"File {str(i+1)} name offset:  {hex(nameoff)}")
 
-                    nameSize = struct.unpack(bom + 'H', inb[nameoff:nameoff + 2])[0]
+                    nameSize = struct.unpack(f'{bom}H', inb[nameoff:nameoff + 2])[0]
                     name = inb[nameoff + 2:nameoff + 2 + nameSize].decode('utf-8')
 
-                    print("File " + str(i+1) + " name:         " + name)
+                    print(f"File {str(i+1)} name:         {name}")
 
                     folder = os.path.dirname(os.path.abspath(filename))
-                    with open(folder + "/" + name, "wb+") as output:
+                    with open(f"{folder}/{name}", "wb+") as output:
                         output.write(data)
 
         else:
